ba1b892d4ec7561aa35fe9729840692584a9937c
Dhruv Arne <dhruvarne22@gmail.com>
2024-12-11 21:42:13 +0530

    Added Email

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -65,7 +65,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "codewithdhruv22@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
